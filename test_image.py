5bbc7c0770d859a8a5d3716a4e9524fef415b287
gogbajbobo <grigoriev.maxim@gmail.com>
2019-02-17 16:48:13 +0300

    export voxels to vtk file

--- test_image.py
+++ test_image.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 voxels = ps.generators.blobs(shape=[10, 10, 10], porosity=0.3, blobiness=2)
 
-print(voxels)
+ps.io.to_vtk(voxels, path='voxels', vox=True)
 
 fig = plt.figure()
 
